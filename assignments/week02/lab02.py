3702fa1550bd8fe5dcf1db838cdd87955ece5b8e
Kittiphat Thongphum <Kittiphat.th@ku.th>
2025-07-15 15:17:41 +0700

    lab02

--- assignments/week02/lab02.py
+++ assignments/week02/lab02.py
@@ -1,12 +1,12 @@
 direction = input("What os ypur conversion direction (1: THB to USD , 2: USD tto THB): ")
-amount = input("Amount to convert: ")
+amount = float(input("Amount to convert: "))
 
 if direction == "1":
     result = amount / 35.5
     print("Result = " ,result)
     print(f"{amount} / 35.5 = {result}")
 
-    if diraction == "2":
+    if direction == "2":
         result = amount * 35.5
         print("Rusult = ",result)
-        print(f"{amount} / 35.5 = {result}")
+        print(f"{amount}/ 35.5 = {result}")
