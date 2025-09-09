cf4376fa7514959d8a8657147e6c2feab8d366bd
Kittiphat Thongphum <kittiphat.th@ku.ac.th>
2025-09-09 15:09:34 +0700

    week7

--- assignments/week07/quiz-2.py
+++ assignments/week07/quiz-2.py
@@ -2,38 +2,44 @@
     สร้าง class Student โดยกำหนดให้
     - มี attribute ชื่อ name, age, และ student_id ที่เก็บข้อมูลทั่วไปของนักเรียน และ grades ที่เก็บคะแนนของนักเรียนในแต่ละวิชา โดยเป็นโครงสร้างข้อมูลประเภท list
     - มี method ชื่อ add_grade(subject, grade) โดย grade เป็น dictionary ที่เก็บคะแนนของนักเรียนในแต่ละวิชา
-        { 
-            "subject": "Mathematics", "grade": 85 
+        {
+            "subject": "Mathematics", "grade": 85
         }
     - มี method ชื่อ get_average_grade() ที่คืนค่าเฉลี่ยคะแนนของนักเรียน
     - มี method ชื่อ get_grade_report() ที่คืนค่ารายงานผลการเรียนของนักเรียน
 """
-
+ 
 class Student:
-    
+   
     def __init__(self, name, age, student_id):
         self.name = name
         self.age = age
         self.student_id = student_id
         self.grades = []
-
+ 
     # Method to add a grade
     def add_grade(self, grade):
-        pass
-
+        self.grades.append(grade)
+ 
     # Method to get the average grade
     def get_average_grade(self):
-        pass
-
+        sum = 0
+        for grade in self.grades:
+            sum = sum + grade['grade']
+        return sum / len(self.grades)
+ 
+ 
     # Method to get the grade report
     def get_grade_report(self):
-        pass
-
-
+        report = ""
+        for grade in self.grades:
+            report = report+ f"Subject:"{grade['subject']}=Grade:{grade['grade']}\n"
+        return report
+ 
 student = Student("John", 20, "S123")
 student.add_grade(
     {
-        "subject": "Math", 
+        "subject": "Math",
         "grade": 85
     }
 )
